d919346279d8f00ce3bc2d3a318e336a683e7c3b
andre-silva366 <andre.java.ads.2022@gmail.com>
2024-10-30 20:08:29 -0300

    Adicionado ConvertTo e Parse

---
+++ ConvertTo/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+try
+{
+    // Conversões com Convert.To
+    WriteLine("-----Convert Números");
+    int n1 = Convert.ToInt16("100");
+    WriteLine($"{n1.GetType()} - {n1}");
+    Int32 n2 = Convert.ToInt32("200500");
+    WriteLine($"{n2.GetType()} - {n2}");
+    Int64 n3 = Convert.ToInt64("1003232131321321");
+    WriteLine($"{n3.GetType()} - {n3}");
+    decimal preco = Convert.ToDecimal("1420,50");
+    WriteLine($"{preco.GetType()} - {preco: n2}");
+    WriteLine();
+    WriteLine("-----Convert String");
+    string texto1 = Convert.ToString(250.59M);
+    WriteLine($"{texto1.GetType()} - { texto1} - resultado: {texto1 ?? "texto é nulo"}");
+    string texto2 = Convert.ToString(DateTime.Today);
+    WriteLine($"{ texto2.GetType()} - { texto2}");
+    WriteLine();
+    WriteLine("-----Convert Boleano");
+    bool valido = Convert.ToBoolean("false");
+    WriteLine($"{ valido.GetType()} - { valido}");
+    WriteLine($"0 - { Convert.ToBoolean(0)}");
+    WriteLine($"1 - { Convert.ToBoolean(1)}");
+    WriteLine($"100 - { Convert.ToBoolean(100)}");
+    WriteLine();
+    WriteLine("-----Convert Data");
+    DateTime natal = Convert.ToDateTime("25 / 12 / 2021");
+    WriteLine($"Natal: { natal.GetType()} - {natal: dd / MMMM / yyyy}");
+    WriteLine($"Natal: { natal.Day} - {natal.Month} - { natal.Year}");
+}
+catch (Exception ex)
+{
+    WriteLine(ex);
+}
+
+ReadKey();
+
---
+++ Parse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using static System.Console;
+
+WriteLine("Conversões com PARSE");
+try
+{
+    WriteLine("----- .Parse");
+    WriteLine($"{Int16.Parse("150")}");
+    WriteLine("-----------------------------");
+    // retorna -150, os parênteses representam o valor negativo
+    WriteLine($"{Int16.Parse("(150)", NumberStyles.AllowParentheses)}");
+    WriteLine("-----------------------------");
+    // retorna 50000 mesmo com o ponto de milhar
+    WriteLine($"{int.Parse("50.000", NumberStyles.AllowThousands)}");
+    WriteLine("-----------------------------");
+    // retorna 50000 mesmo com o simbolo monetário
+    WriteLine($"Cultura atual: {CultureInfo.CurrentCulture.Name}");
+    WriteLine("-----------------------------");
+    WriteLine($"Símbolo da moeda: {NumberFormatInfo.CurrentInfo.CurrencySymbol}");
+    WriteLine("-----------------------------");
+    // retorna -200 mesmo com o símbolo de negativo
+    WriteLine($"{int.Parse("-200", NumberStyles.AllowLeadingSign)}");
+    WriteLine("-----------------------------");
+    // retorna 200 mesmo com os espaçõs em branco antes e depois
+    WriteLine($"{int.Parse(" 100 ", NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite)}");
+    WriteLine("-----------------------------");
+    // retorna 5432123456
+    WriteLine($"{Int64.Parse("5432123456")}");
+    WriteLine("-----------------------------");
+    WriteLine($"Data: {DateTime.Parse("21/4/2021"):dd/MMM/yyyy}");
+}
+catch (Exception ex)
+{
+    WriteLine(ex);
+}
+
+ReadKey();
