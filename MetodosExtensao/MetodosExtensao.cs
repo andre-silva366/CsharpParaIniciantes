c5bc9dca239a028674a50a5298d99af8e750e273
andre-silva366 <andre.java.ads.2022@gmail.com>
2024-11-01 14:03:06 -0300

    Adicionado Metodos de Extensao

---
+++ MetodosExtensao/MetodosExtensao.cs
@@ -0,0 +1,8 @@
+﻿namespace MetodosExtensao;
+
+public static class MetodosExtensao
+{
+    // o método de extensão deve set static
+    public static string FormatarData(this DateTime data, string formato) => data.ToString(formato);
+    public static string Formatar(this decimal valor, string formato) => valor.ToString(formato);
+}
---
+++ MetodosExtensao/Program.cs
@@ -0,0 +1,13 @@
+﻿using MetodosExtensao;
+using static System.Console;
+
+WriteLine(DateTime.Today.FormatarData("dd/MMM/yyyy"));
+WriteLine(DateTime.Today.FormatarData("dddd dd/MMM/yyyy"));
+WriteLine(DateTime.Today.FormatarData("yyyy"));
+
+decimal valor = 5800.78m;
+
+WriteLine(valor.Formatar("R$ ##,##0.00"));
+WriteLine($"{4578.87M.Formatar("C2")}");
+ReadLine();
+
