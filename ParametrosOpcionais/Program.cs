890f581e708bebb1ce2a5faceb8bea4d3ab102e8
andre-silva366 <andre.java.ads.2022@gmail.com>
2024-11-01 18:38:51 -0300

    Adicionado Parametros Opcionais

---
+++ ParametrosOpcionais/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+WriteLine("Parâmetros Opcionais");
+WriteLine(ParametrosOpcionais.Faturamento(500,5,10));
+WriteLine("Parâmetros 1 e 2");
+WriteLine(ParametrosOpcionais.Faturamento(50, 12));
+WriteLine("Parâmetros 1 e 3"); 
+WriteLine(ParametrosOpcionais.Faturamento(50, bonus: 12));
+ReadLine();
+
+public static class ParametrosOpcionais
+{
+    // regra : obrigatórios DEVEM vir antes dos opcionais
+   public static decimal Faturamento(decimal qtde, decimal preco = 10, decimal bonus = 5) => qtde * preco + bonus;
+}
+
+
+
