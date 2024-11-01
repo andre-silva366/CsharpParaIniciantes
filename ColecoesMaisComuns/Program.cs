58d7d0fefb365ef7cd6a5ab7f423b4b34277eb5c
andre-silva366 <andre.java.ads.2022@gmail.com>
2024-11-01 13:47:02 -0300

    Adicionad Colecoes Mais Usadas

---
+++ ColecoesMaisComuns/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+//// Declarando um array com capacidade definida mas sem valores
+//int[] primeiroArray = new int[100];
+
+//// Declarando um array e já atribuindo valores
+//int[] segundoArray = new int[] { 1, 3, 5, 7, 9 };
+
+//WriteLine("Valor do array na posição 0 -> {0}", segundoArray[0]);
+
+//// Forma alternativa de criar array
+//int[] terceiroArray = { 1, 2, 3, 4, 5, 6 };
+
+//// Declarando um array multi-dimensional
+//int[,] arrayMultiDimensional = new int[2, 3];
+
+//// Declarando um array multi-dimensional e atribuindo valores
+//int[,] arrayMultiDimensional2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+// Lista
+
+//List<string> nomesFuncionarios = new List<string>();
+//nomesFuncionarios.Add("Maria");
+//nomesFuncionarios.Add("João");
+//nomesFuncionarios.Add("André");
+//nomesFuncionarios.Add("Flávia");
+
+//WriteLine("Lista completa");
+
+//foreach (var pessoa in nomesFuncionarios)
+//{
+//    WriteLine(pessoa);
+//}
+
+//WriteLine("\nAcessando o primeiro elemento da lista");
+
+//WriteLine(nomesFuncionarios[0]);
+
+//// Removendo o primeiro elemento da lista
+//nomesFuncionarios.RemoveAt(0);
+
+//WriteLine("\nAcessando o primeiro elemento da lista após remover");
+
+//WriteLine(nomesFuncionarios[0]);
+
+ReadLine();
+
