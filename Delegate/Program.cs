46f84a1115c9ddc0187634a1b64eaa078df2f979
andre-silva366 <andre.java.ads.2022@gmail.com>
2024-11-01 21:07:30 -0300

    Adicionado Delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+// Func < até 16 entradas e UMA saída >
+int contador = 0;
+Func<int> numero = () => ++contador;
+WriteLine($"chamada 1 : {numero()}");
+WriteLine($"chamada 2 : {numero()}");
+WriteLine($"contador : {contador}");
+
+// Func contador de caracteres
+Func<string, int> contadorCaracteres = (string palavra) => palavra.Length;
+WriteLine(contadorCaracteres("Visual C#"));
+
+// Quantidade de palavras de uma expressão
+Func<string, int> contaPalavras = (string texto) => texto.Split(' ').Where(x => x != "").Count();
+WriteLine(contaPalavras("       Brasil  campeão            mundial     de     volei"));
+
+// calcular o imposto
+// salario, perc, aliquota
+// formula: salario * (perc/100) - aliquota
+// condicao: salario <= 1000 >>> imposto = 0
+// cond ? true : false
+
+Func<decimal, decimal, decimal, decimal> imposto = (salario, percentual, aliquota) =>
+{
+    return salario <= 100 ? 0 : salario * (percentual / 100) - aliquota;
+};
+
+WriteLine("---- Calculo do imposto ---");
+WriteLine(imposto(1000, 10, 10).ToString("c2"));
+WriteLine(imposto(5000, 27.5m, 80).ToString("c2"));
+WriteLine(imposto(23500, 32.5m, 180).ToString("c2"));
+
+
+ReadLine();
+
